bffb80dc3efe861c66cb3666ae8785d4f0a82c8c
MaskQ <and23520000@gmail.com>
2025-04-12 19:19:44 +0800

    feat: thug hack create max thread

--- src/thug.ts
+++ src/thug.ts
@@ -2,18 +2,22 @@ import { NS } from "@ns";
 /** @param {NS} ns */
 export async function main(ns: NS) {
     const serverHostnames = (ns.args[0] as string).split(',');
-    const scriptUsedRam = ns.getScriptRam('/lib/hack.js')
+    const hackScriptRam = ns.getScriptRam('/lib/hack.js')
+    let i = 0
+    const hostNumber = serverHostnames.length
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        const hostname = serverHostnames[i % (hostNumber - 1)];
+        const freeRam = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)
 
-    while (serverHostnames.length > 0) {
-        for (let i = 0; i < serverHostnames.length; i++) {
-            const hostname = serverHostnames[i];
-            const freeRam = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)
-            if (freeRam >= scriptUsedRam) {
-                ns.tprint(hostname);
-               const pid =  ns.run('/lib/hack.js', { threads: 1 }, hostname);
-               //remove host from list
-               if(pid > 0) serverHostnames.splice(i, 1); 
-            }
+        if (freeRam >= hackScriptRam) {
+            ns.tprint(hostname);
+             ns.run('/lib/hack.js', { threads: 1 }, hostname);
+             i++
+        }else{
+            break;
         }
     }
 }
+
+
