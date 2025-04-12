cdd45a232a455c4b5973501715da7016f59e023d
MaskQ <and23520000@gmail.com>
2025-04-12 22:36:39 +0800

    feat: add syncRun lib

---
+++ src/lib/syncRun.ts
@@ -0,0 +1,16 @@
+import { NS } from "@ns";
+
+export async function syncRun(ns: NS, script: string, hostname: string) {
+    const pid = ns.run(script, { threads: 1 }, hostname)
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        const allPs = ns.ps()
+        const ps = allPs.find(p => p.pid === pid)
+        if (!ps) {
+            ns.tprintf(`script ${script} has finished`)
+            break
+        }
+        await ns.sleep(500)
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,11 @@
 import { NS } from "@ns";
+import { syncRun } from "./lib/syncRun";
 /** @param {NS} ns */
 export async function main(ns: NS) {
     const hostname = ns.getHostname()
     ns.killall()
     ns.tprintf(`Starting rooter on ${hostname}`)
-    const rooterPid = ns.run('/rooter.js', { threads: 1 }, hostname)
-
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        const ps = ns.ps()
-        const rootPs = ps.find(p => p.pid === rooterPid)
-        if (!rootPs) {
-            ns.tprintf(`Rooter on ${hostname} has finished`)
-            break
-        }
-        await ns.sleep(1000)
-    }
+    await syncRun(ns, '/rooter.js', hostname)
     ns.tprintf(`Daddy is active on ${hostname}`)
     await ns.run('/daddy.js', { threads: 1 }, hostname)
 }
